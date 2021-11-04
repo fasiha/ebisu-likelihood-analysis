bfc729e6d4741e84f3c3655b25021e7a3cd2439d
Ahmed Fasih <wuzzyview@gmail.com>
2021-11-03 23:45:00 -0700

    WIP: added simple update binomial

--- ebisu3.py
+++ ebisu3.py
@@ -1,7 +1,8 @@
 import numpy as np
-from scipy.special import kv, kve, gamma
+from scipy.special import kv, kve, gamma, betaln, logsumexp  #type: ignore
 from dataclasses import dataclass, replace
 from time import time_ns
+from functools import cache
 from typing import Union
 
 
@@ -9,9 +10,10 @@ from typing import Union
 class Model:
   elapseds: list[float]  # your choice of time units; possibly empty
 
-  # ints: binomial quizzes. Floats: fuzzy noisy-Bernoulli
+  # First element: ints (binomial quizzes) or floats (fuzzy noisy-Bernoulli)
+  # second element: total number of trials (1 implies binary, fuzzy or not-fuzzy)
   # same length as `elapseds`
-  results: list[Union[float, int]]
+  results: list[tuple[Union[float, int], int]]
 
   startStrengths: list[float]  # 0 < x <= 1 (reinforcement). Same length as `elpseds`
 
@@ -31,6 +33,12 @@ def _meanVarToGamma(mean, var) -> tuple[float, float]:
   a = mean**2 / var
   b = mean / var
   return a, b
+
+
+def _logmeanlogVarToGamma(logmean, logvar) -> tuple[float, float]:
+  loga = 2 * logmean - logvar
+  logb = logmean - logvar
+  return np.exp(loga), np.exp(logb)
 
 
 def _timeMs() -> float:
@@ -68,7 +76,63 @@ def _simpleUpdateNoisy(model: Model,
   ret = replace(model)  # clone
   ret.halflifePrior = (newAlpha, newBeta)
   ret.elapseds.append(elapsed)
-  ret.results.append(result)
+  ret.results.append((result, 1))
+  ret.startStrengths.append(reinforcement)
+  if reinforcement > 0:
+    ret.halflife = mean
+    ret.startTime = now or _timeMs()
+    ret.logStrength = np.log(reinforcement)
+  else:
+    ret.halflife = mean
+
+  return ret
+
+
+@cache
+def _binomln(n, k):
+  "Log of scipy.special.binom calculated entirely in the log domain"
+  return -betaln(1 + n - k, 1 + k) - np.log(n + 1)
+
+
+def _simpleUpdateBinomial(model: Model,
+                          elapsed: float,
+                          successes: int,
+                          total: int,
+                          now: Union[None, float] = None,
+                          reinforcement: float = 1.0) -> Model:
+  LN2 = np.log(2)
+
+  def logs(a: float, b: float, c: float):
+    # s = lambda a, b, c: 2 * (c / b)**(a * 0.5) * kv(-a, 2 * np.sqrt(b * c))
+    # `kve = kv * exp(z)` -> `log(kve) = log(kv) + z` -> `log(kv) = log(kve) - z`
+    z = 2 * np.sqrt(b * c)  # arg to kv
+    return LN2 + np.log(c / b) * (a * 0.5) + np.log(kve(a, z)) - z
+
+  k = successes
+  n = total
+  a, b = model.halflifePrior
+  t = elapsed
+
+  def logmoment(nth) -> float:
+    loglik = []
+    scales = []
+    for i in range(0, n - k + 1):
+      loglik.append(_binomln(n, i) + logs(a + nth, b, t * (k + i)))
+      scales.append((-1)**i)
+    return logsumexp(loglik, b=scales)
+
+  logm0 = logmoment(0)
+  logmean = logmoment(1) - logm0
+  logm2 = logmoment(2) - logm0
+  logvar = logsumexp([logm2, 2 * logmean], b=[1, -1])
+  newAlpha, newBeta = _logmeanlogVarToGamma(logmean, logvar)
+
+  mean = np.exp(logmean)
+
+  ret = replace(model)  # clone
+  ret.halflifePrior = (newAlpha, newBeta)
+  ret.elapseds.append(elapsed)
+  ret.results.append((successes, total))
   ret.startStrengths.append(reinforcement)
   if reinforcement > 0:
     ret.halflife = mean
@@ -90,5 +154,6 @@ def simpleUpdate(model: Model,
   if total == 1 and (0 < successes < 1):
     return _simpleUpdateNoisy(
         model, elapsed, successes, now=now, q0=q0, reinforcement=reinforcement)
-  ret = replace(model)  # clone
-  return ret
+  assert successes == np.floor(successes), "float `successes` implies `total==1`"
+  return _simpleUpdateBinomial(
+      model, elapsed, int(successes), total, now=now, reinforcement=reinforcement)
