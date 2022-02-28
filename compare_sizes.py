dd97a02a792a56141d4139986a85f5f66e3745e7
Ahmed Fasih <wuzzyview@gmail.com>
2022-02-27 23:05:19 -0800

    comparing full Ebisu update 1k vs 100k

---
+++ compare_sizes.py
@@ -0,0 +1,38 @@
+import ebisu3 as ebisu
+import utils
+
+initHlMean = 10  # hours
+initHlBeta = 0.1
+initHlPrior = (initHlBeta * initHlMean, initHlBeta)
+
+boostMean = 1.5
+boostBeta = 3.0
+boostPrior = (boostBeta * boostMean, boostBeta)
+
+model = ebisu.initModel(initHlPrior, boostPrior)
+model = ebisu.simpleUpdateRecall(model, 15.0, 1)
+model = ebisu.simpleUpdateRecall(model, 35.0, 0)
+model = ebisu.simpleUpdateRecall(model, 25.0, 1)
+model = ebisu.simpleUpdateRecall(model, 45.0, 0.9)
+
+
+def summarize(big: ebisu.Model, small: ebisu.Model):
+  for bigsmall in ['big', 'small']:
+    for rv in ['initHl', 'boost']:
+      this = (big if bigsmall == 'big' else small)
+      post = this.prob.initHl if rv == "initHl" else this.prob.boost
+      mean = ebisu._gammaToMean(*post)
+      print(f'{bigsmall} | {rv} | {post} | {mean:0.3f} | {this.pred.currentHalflife:0.3f}')
+
+
+summarize(ebisu.fullUpdateRecall(model, size=100_000), ebisu.fullUpdateRecall(model, size=1_000))
+
+df = utils.sqliteToDf('collection.anki2', True)
+print(f'loaded SQL data, {len(df)} rows')
+train, TEST_TRAIN = utils.traintest(df)
+t = next(t for t in train if t.fractionCorrect > 0.8)
+real = ebisu.initModel(initHlPrior, boostPrior)
+for x, t in zip(t.results, t.dts_hours):
+  real = ebisu.simpleUpdateRecall(real, t, int(x >= 2))
+
+summarize(ebisu.fullUpdateRecall(real, size=100_000), ebisu.fullUpdateRecall(real, size=1_000))
